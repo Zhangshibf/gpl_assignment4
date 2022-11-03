7ed39041254b8e794c23bcdf680789efd5d7c8f6
zhangshibf <zhangshibf@gmail.com>
2022-11-03 14:25:15 +0100

    a

--- caption.py
+++ caption.py
@@ -111,6 +111,7 @@ def caption_image_beam_search(encoder, decoder, image_path, word_map, beam_size=
         seqs = torch.cat([seqs[prev_word_inds.long()], next_word_inds.unsqueeze(1)], dim=1)  # (s, step+1)
         seqs_alpha = torch.cat([seqs_alpha[prev_word_inds.long()], alpha[prev_word_inds.long()].unsqueeze(1)],
                                dim=1)  # (s, step+1, enc_image_size, enc_image_size)
+        #a
 
         # Which sequences are incomplete (didn't reach <end>)?
         incomplete_inds = [ind for ind, next_word in enumerate(next_word_inds) if
